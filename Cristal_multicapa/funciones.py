e5114f6204cc7c29e15084f23a59293da6b332d5
Fran <franfi17sw@gmail.com>
2024-03-06 19:17:26 +0100

    añadido multicapa

--- Cristal_multicapa/funciones.py
+++ Cristal_multicapa/funciones.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def snell_law(n1, n2, theta1):
     '''
     Ley de Snell, calcula el angulo de refracción
@@ -11,39 +12,43 @@ def snell_law(n1, n2, theta1):
     return theta2: refraction angle    
     '''
 
-    stheta2 = n1*np.sin(theta1)/n2 
+    stheta2 = n1*np.sin(theta1)/n2
 
     return np.arcsin(stheta2)
+
 
 def inter(n, theta):
     '''
     Calcula la matriz para la interfase.
-    
+
     n: refraction index
     theta: angle
 
     return D: matrix
     '''
 
-    D =  np.array([[1,1],
-            [n*np.cos(theta), -n*np.cos(theta)]])
+    D = np.array([[1, 1],
+                  [n*np.cos(theta), -n*np.cos(theta)]])
 
     return D
 
+
 def prop(theta, d, n, wavelenth):
     '''
-    
+
     '''
 
     phi = n*2*np.pi/wavelenth*np.cos(theta)*d
 
     P = np.array([[np.exp(1j*phi), 0],
-                  [0, -np.exp(-1j*phi)]])
+                  [0, np.exp(-1j*phi)]])
     return P
+
+
 def coef_form(n1, n2, theta1, theta2):
     '''
     Calcula los coeficientes de transmisión y reflexión
-    
+
     n1: refraction index
     n2: refraction index
     theta1: incidence angle
@@ -52,15 +57,16 @@ def coef_form(n1, n2, theta1, theta2):
     return t: transmission coefficient
     return r: reflection coefficient
     '''
-    
+
     c1 = np.cos(theta1)
     c2 = np.cos(theta2)
 
     r = (n1*c1 - n2*c2)/(n1*c1 + n2*c2)
 
     t = 2*n1*c1/(n1*c1 + n2*c2)
-    
+
     return t, r
+
 
 def coef_monocapa(n1, n2, n3, d, theta1, theta2, theta3, wavel):
     '''
@@ -72,7 +78,19 @@ def coef_monocapa(n1, n2, n3, d, theta1, theta2, theta3, wavel):
 
     phi = n2*2*np.pi*d/wavel*np.cos(theta2)
 
-    t = t12*t23*np.exp(1j*phi)/(1 + r12*r23*np.exp(-2j*phi))
-    r = r12 * r23 * np.exp(-2j*phi)/(1 + r12*r23*np.exp(-2j*phi))
+    t = t12*t23*np.exp(-1j*phi)/(1 + r12*r23*np.exp(-2*1j*phi))
+    r = (r12 + r23 * np.exp(-2*1j*phi))/(1 + r12*r23*np.exp(-2*1j*phi))
 
     return t, r
+
+
+def coef_TR(t, r, theta0, thetaf, n0, nf):
+    '''
+    '''
+
+    T = nf/n0*np.abs(t)**2*np.cos(thetaf)/np.cos(theta0)
+    R = np.abs(r)**2
+
+    return T, R
+
+
--- Cristal_multicapa/main.py
+++ Cristal_multicapa/main.py
@@ -5,24 +5,36 @@ Optica de multicapa
 import numpy as np
 import funciones as fn
 
+# Unidades en nanómetros
+
 n1 = 1.0
 n2 = 1.5
+n3 = 1.0
+d = 100.0
+wavel = 440
+
+
 theta1 = np.pi/2
 
 theta2 = fn.snell_law(n1, n2, theta1)
 
+theta3 = fn.snell_law(n2, n3, theta2)
+
 D1 = fn.inter(n1, theta1)
 D2 = fn.inter(n2, theta2)
+D3 = fn.inter(n3, theta3)
 
 D1inv = np.linalg.inv(D1)
+D2inv = np.linalg.inv(D2)
+P2 = fn.prop(n2, d, theta2, wavel)
 
-dtot = D1inv @ D2
+dtot = D1inv @ D2 @ P2 @ D2inv @ D3
 
 t = 1/dtot[0,0]
-
 r = dtot[1,0]/dtot[0,0]
 
-ta, ra = fn.coef_form(n1, n2, theta1, theta2)
+ta, ra = fn.coef_monocapa(n1, n2, n3, d, theta1, theta2, theta3, wavel)
 
 print(f't = {t}, ta = {ta} \n r = {r}, ra = {ra}')
+
 
